2e309c876758be4723181170f6a5997482ab1ff0
arthur <arthur.spinosa12@gmail.com>
2026-04-29 18:14:05 -0300

    ✨ feat: adiciona componentes iniciais de lista e detalhe de imbuimentos

---
+++ src/app/detalhe-do-imbuimento/detalhe-do-imbuimento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-do-imbuimento',
+  imports: [],
+  templateUrl: './detalhe-do-imbuimento.component.html',
+  styleUrl: './detalhe-do-imbuimento.component.css'
+})
+export class DetalheDoImbuimentoComponent {
+
+}
---
+++ src/app/lista-de-imbuimentos/lista-de-imbuimentos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-imbuimentos',
+  imports: [],
+  templateUrl: './lista-de-imbuimentos.component.html',
+  styleUrl: './lista-de-imbuimentos.component.css'
+})
+export class ListaDeImbuimentosComponent {
+
+}
