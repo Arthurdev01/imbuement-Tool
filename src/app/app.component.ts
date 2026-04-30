4be4c91ba434351a1f34600bd8ce821711dd5684
arthur <arthur.spinosa12@gmail.com>
2026-04-30 15:33:10 -0300

    ♻️ refactor: substitui template inicial pelo componente de lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaDeImbuimentosComponent } from './lista-de-imbuimentos/lista-de-imbuimentos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,ListaDeImbuimentosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
