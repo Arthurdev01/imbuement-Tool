b927c82a86c807e7fc4ecba02105990f2db2e8fe
arthur <arthur.spinosa12@gmail.com>
2026-04-30 15:31:15 -0300

    ✨ feat: adiciona serviço de imbuimentos

---
+++ src/app/imbuiment.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImbuimentService {
+  
+imbuimentos = [
+
+  {
+    nome: "Strike (Critical Hit)",
+    itens:[{ nomeItem: "Protective Charm", quantidade: 20 }, { nomeItem: "Sabretooth", quantidade: 25 }, { nomeItem: "Vexclaw Talon", quantidade: 5 }]
+  },{
+    nome: "Vampirism (Life Leech)",
+    itens:[{ nomeItem: "Vampire Teeth", quantidade: 25 }, { nomeItem: "Bloody Pincers", quantidade: 15 }, { nomeItem: "Piece of Dead Brain", quantidade: 5 }]
+  },{
+    nome: "Void (Mana Leech)",
+    itens:[{ nomeItem: "Rope Belts", quantidade: 25 }, { nomeItem: "Silencer Claws", quantidade: 25 }, { nomeItem: "Some Grimeleech Wings", quantidade: 5 }]
+  }
+
+];
+
+  constructor() { }
+}
