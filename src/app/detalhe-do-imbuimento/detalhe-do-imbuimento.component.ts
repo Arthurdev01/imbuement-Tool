33178cb1788302e936ff8a38dd632da8c9c31d21
arthur <arthur.spinosa12@gmail.com>
2026-04-30 18:13:13 -0300

    ✨ feat: exibe detalhes do imbuimento selecionado

--- src/app/detalhe-do-imbuimento/detalhe-do-imbuimento.component.ts
+++ src/app/detalhe-do-imbuimento/detalhe-do-imbuimento.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ImbuimentService } from '../../service/imbuiment.service';
 
 @Component({
   selector: 'app-detalhe-do-imbuimento',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detalhe-do-imbuimento.component.html',
   styleUrl: './detalhe-do-imbuimento.component.css'
 })
 export class DetalheDoImbuimentoComponent {
-
+@Input() imbuimentSelecionado: any = null;
 }
