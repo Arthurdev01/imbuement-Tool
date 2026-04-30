d4c0754eb4c7595bc0881283d4d79d45ef013b3a
arthur <arthur.spinosa12@gmail.com>
2026-04-30 15:32:15 -0300

    ✨ feat: exibe lista de imbuimentos

--- src/app/lista-de-imbuimentos/lista-de-imbuimentos.component.ts
+++ src/app/lista-de-imbuimentos/lista-de-imbuimentos.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ImbuimentService } from '../imbuiment.service';
 
 @Component({
   selector: 'app-lista-de-imbuimentos',
-  imports: [],
+  imports: [CommonModule,NgFor],
   templateUrl: './lista-de-imbuimentos.component.html',
   styleUrl: './lista-de-imbuimentos.component.css'
 })
 export class ListaDeImbuimentosComponent {
 
+constructor(public imbuiment: ImbuimentService){
+this.imbuiment.imbuimentos
 }
+
+detalhes() {
+    console.log('Button clicked!');
+  }
+
+}
+interface item{
+  nome:string;
+  quantidade: number
+}
+interface  imbuimento{
+nome: string;
+itens: item[];
+}
+
+
+
