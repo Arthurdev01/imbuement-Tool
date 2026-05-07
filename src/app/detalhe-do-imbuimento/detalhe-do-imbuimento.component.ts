9f408ca9ca0d0478ad1a3b43c669f4698cd2b762
arthur <arthur.spinosa12@gmail.com>
2026-05-07 00:16:12 -0300

    ✨ feat(imbuimento): comparar custos entre market e gold token

--- src/app/detalhe-do-imbuimento/detalhe-do-imbuimento.component.ts
+++ src/app/detalhe-do-imbuimento/detalhe-do-imbuimento.component.ts
@@ -21,6 +21,18 @@ maisBarato(item: any){
   return resultado
 }
 
+quantosComToken(){
+let contador = 0
+for (const item of this.imbuimentSelecionado.itens) {
+    if ( this.maisBarato(item) === "Comprar pelo Market" ) {
+        break;
+    }else{
+      contador++
+    }
+}
+return contador
+}
+
 calcularTotal(){
  return this.imbuimentSelecionado?.itens.reduce((acumulador: number, item: any) => acumulador + ((item.preco ?? 0) * item.quantidade),0);
 }
@@ -36,4 +48,15 @@ calcularTotalTaxaGold(){
  return this.calcularTotalGoldToken() +250000;
 }
 
+qualMaisBarato(){
+Math.abs(this.calcularTotalTaxaGold() - this.calcularTotalTaxa())
+
+ if(this.calcularTotalTaxaGold() < this.calcularTotalTaxa()){
+   return { melhor: "Gold Token", diferenca: Math.abs(this.calcularTotalTaxaGold() - this.calcularTotalTaxa())
+ }
+ }else{
+return { melhor: "Market", diferenca: Math.abs(this.calcularTotalTaxaGold() - this.calcularTotalTaxa())
+ }
+}
+}
 }
