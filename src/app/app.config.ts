882f9c2251d11751070e0de86d0f037aa3b8d16d
arthur <arthur.spinosa12@gmail.com>
2026-05-07 21:16:06 -0300

    📦 build(deps): adicionar suporte a máscaras e ícones

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection
+} from '@angular/core';
+
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideNgxMask } from 'ngx-mask';
+
+import { LucideAngularModule, Copy } from 'lucide-angular';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({
+      eventCoalescing: true
+    }),
+
+    provideRouter(routes),
+
+    provideNgxMask(),
+
+    importProvidersFrom(
+      LucideAngularModule.pick({ Copy })
+    )
+  ]
 };
