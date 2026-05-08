41e5a8fcee24df9a72f327c2c8322bfb39684779
arthur <arthur.spinosa12@gmail.com>
2026-05-07 21:17:09 -0300

    ✨ feat(imbuimentos): adicionar tipo visual aos imbuimentos

--- src/service/imbuiment.service.ts
+++ src/service/imbuiment.service.ts
@@ -5,20 +5,23 @@ import { Injectable } from '@angular/core';
 })
 export class ImbuimentService {
 
-imbuimentos = [
-
-  {
-    nome: "Strike (Critical Hit)",
-    itens:[{ nomeItem: "Protective Charm", quantidade: 20, preco: null }, { nomeItem: "Sabretooth", quantidade: 25, preco: null }, { nomeItem: "Vexclaw Talon", quantidade: 5, preco: null }]
-  },{
-    nome: "Vampirism (Life Leech)",
-    itens:[{ nomeItem: "Vampire Teeth", quantidade: 25, preco: null}, { nomeItem: "Bloody Pincers", quantidade: 15, preco: null }, { nomeItem: "Piece of Dead Brain", quantidade: 5, preco: null }]
-  },{
-    nome: "Void (Mana Leech)",
-    itens:[{ nomeItem: "Rope Belts", quantidade: 25, preco: null }, { nomeItem: "Silencer Claws", quantidade: 25, preco: null }, { nomeItem: "Some Grimeleech Wings", quantidade: 5, preco: null }]
-  }
-
-];
+  imbuimentos = [
+    {
+      nome: "Strike (Critical Hit)",
+      tipo: "strike",
+      itens: [{ nomeItem: "Protective Charm", quantidade: 20, preco: null }, { nomeItem: "Sabretooth", quantidade: 25, preco: null }, { nomeItem: "Vexclaw Talon", quantidade: 5, preco: null }]
+    },
+    {
+      nome: "Vampirism (Life Leech)",
+      tipo: "vampirism",
+      itens: [{ nomeItem: "Vampire Teeth", quantidade: 25, preco: null }, { nomeItem: "Bloody Pincers", quantidade: 15, preco: null }, { nomeItem: "Piece of Dead Brain", quantidade: 5, preco: null }]
+    },
+    {
+      nome: "Void (Mana Leech)",
+      tipo: "void",
+      itens: [{ nomeItem: "Rope Belts", quantidade: 25, preco: null }, { nomeItem: "Silencer Claws", quantidade: 25, preco: null }, { nomeItem: "Some Grimeleech Wings", quantidade: 5, preco: null }]
+    }
+  ];
 
   constructor() { }
 }
